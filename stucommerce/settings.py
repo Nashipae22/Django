946a102f793c9ff6a3fa9d46838b22b4b74794ea
Nashipae <flanyasunya274@gmail.com>
2024-10-01 14:24:38 -0700

    port

--- stucommerce/settings.py
+++ stucommerce/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,3 +133,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+if 'runserver' in sys.argv:
+    port=sys.argv[-1]
+
+    if port=="8000":
+        SESSION_COOKIE_NAME="user_session"
+
+    elif port=="8080":
+        SESSION_COOKIE_NAME="admin_session"
